6f180e38129eb8a327f3df93c3d974cc93f436b5
jeny <v8199@yandex.ru>
2016-04-30 02:22:53 +0600

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,8 @@
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list' -y
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 
-apt-get -y update && apt-get -y install postgresql postgresql-contrib
-
+apt-get -y update > /dev/null
+apt-get -y install postgresql postgresql-contrib
 
 # fix access type
 echo "-------------------- fixing postgres pg_hba.conf file"
@@ -45,6 +45,6 @@ gem install bundler
 
 #install nodejs features
 
-add-apt-repository ppa:chris-lea/node.js
-apt-get -y update
+add-apt-repository ppa:chris-lea/node.js > /dev/null
+apt-get -y update > /dev/null
 apt-get -y install nodejs
