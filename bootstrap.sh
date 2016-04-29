f42d34635357b4e1b8d8b9a69b738575802bfa69
jeny <v8199@yandex.ru>
2016-04-30 03:20:38 +0600

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,6 +30,7 @@ echo '- Install rbenv'
 apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev > /dev/null
 
 cd /home/vagrant && git clone git://github.com/sstephenson/rbenv.git .rbenv
+#TODO: Can't create automatically
 echo 'export PATH="/home/vagrant/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
