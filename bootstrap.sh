aa58c54246651a6ad9f675d3feebfc30744c43c9
gfsx0259 <v8199@yandex.ru>
2016-05-02 10:28:34 +0600

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,20 +2,18 @@
 
 echo '- Install postgresql 9.5'
 
-sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list' > /dev/null
+sudo wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add - > /dev/null
 
-apt-get -y update > /dev/null
-apt-get -y install postgresql postgresql-contrib > /dev/null
-
+sudo apt-get -y update > /dev/null
+sudo apt-get -y install postgresql postgresql-contrib > /dev/null
 
 psql --version
-
 
 # fix access type
 echo "- Fixing postgres pg_hba.conf file"
 # replace the ipv4 host line with the above line
-cat >> /etc/postgresql/9.5/main/pg_hba.conf <<EOF
+sudo cat >> /etc/postgresql/9.5/main/pg_hba.conf <<EOF
 host    all         all         0.0.0.0/0             md5
 EOF
 
@@ -31,18 +29,17 @@ sudo su postgres -c "psql -c \"update pg_database set datistemplate=true where d
 
 echo '- Install rbenv'
 
-apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev > /dev/null
+sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev > /dev/null
 
-cd /home/vagrant && git clone git://github.com/sstephenson/rbenv.git .rbenv
-#TODO: Can't create automatically
-echo 'export PATH="/home/vagrant/.rbenv/bin:$PATH"' >> ~/.bash_profile
+cd $HOME && git clone git://github.com/sstephenson/rbenv.git .rbenv
+
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build > /dev/null
-echo 'export PATH="/home/vagrant/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
 source ~/.bash_profile
 
-#sudo chown -R vagrant .rbenv
 
 echo '- Download and install ruby 2.3.0 build'
 rbenv install -v 2.3.0 > /dev/null
@@ -54,6 +51,6 @@ gem install bundler > /dev/null
 
 echo '- Install nodejs features'
 
-add-apt-repository ppa:chris-lea/node.js > /dev/null
-apt-get -y update > /dev/null
-apt-get -y install nodejs > /dev/null
+sudo add-apt-repository ppa:chris-lea/node.js > /dev/null
+sudo apt-get -y update > /dev/null
+sudo apt-get -y install nodejs > /dev/null
